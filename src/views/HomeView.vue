1ec330b5742a353bea97c9e0fc002191b66fd96c
Faruoq01 <faruk.aminu@mabevic.com>
2022-06-11 08:47:12 +0100

    dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,29 @@ import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home-container">
+    <div class="side-menu">first</div>
+    <div class="main-body">second</div>
+  </div>
 </template>
+
+<style scoped>
+  .home-container{
+    width: 100vw;
+    height:100vh;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .side-menu{
+    width: 15%;
+    height: 100%;
+    background-color:#404690;
+  }
+
+  .main-body{
+    width: 85%;
+    height: 100%;
+    background-color: #fff;
+  }
+</style>
