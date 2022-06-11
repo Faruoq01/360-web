2885c116e4ff8b2796643291e0883e4917200207
Faruoq01 <faruk.aminu@mabevic.com>
2022-06-11 09:34:54 +0100

    new project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,29 +3,7 @@ import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
-  <div class="home-container">
-    <div class="side-menu">first</div>
-    <div class="main-body">second</div>
-  </div>
+  <main>
+    <TheWelcome />
+  </main>
 </template>
-
-<style scoped>
-  .home-container{
-    width: 100vw;
-    height:100vh;
-    display: flex;
-    flex-direction: row;
-  }
-
-  .side-menu{
-    width: 15%;
-    height: 100%;
-    background-color:#404690;
-  }
-
-  .main-body{
-    width: 85%;
-    height: 100%;
-    background-color: #fff;
-  }
-</style>
