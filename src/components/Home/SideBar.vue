ddc047773669197869de7aff8a02f0b9a710c33f
Faruoq01 <faruk.aminu@mabevic.com>
2022-06-13 16:16:34 +0100

    side bar icons replaced

--- src/components/Home/SideBar.vue
+++ src/components/Home/SideBar.vue
@@ -6,9 +6,10 @@
     </div>
 
     <RouterLink class="route-style" to="/">
-        <div class="item-container2" @click="dashboard" v-bind:style="activeStates.A? itemContainer: itemContainer2">
+        <div @click="dashboard" v-bind:style="activeStates.A? itemContainer: itemContainer2">
             <div v-bind:style="activeStates.A? innerContainer: innerContainer2">
-                <img class="dash" src="../../assets/images/dashIcon.png" />
+                <img class="dash" v-if="activeStates.A" src="../../assets/images/dashIcon.png" />
+                <img class="dash" v-else src="../../assets/images/icons/dashWhite.png" />
                 <div class="dash-text" :style="activeStates.A? dashText:dashText2">Dashboard</div>
             </div>
         </div>
@@ -17,7 +18,8 @@
     <RouterLink class="route-style" to="/settings">
         <div class="item-container2" @click="settings" v-bind:style="activeStates.B? itemContainer: itemContainer2">
             <div v-bind:style="activeStates.B? innerContainer: innerContainer2">
-                <img class="dash" src="../../assets/images/settings.png" />
+                <img class="dash" v-if="activeStates.B" src="../../assets/images/icons/setWhite.png" />
+                <img class="dash" v-else src="../../assets/images/settings.png" />
                 <div class="dash-text2" :style="activeStates.B? dashText:dashText2">Settings</div>
             </div>
         </div>
@@ -26,7 +28,8 @@
     <RouterLink class="route-style" to="/feeds">
         <div class="item-container2" @click="feeds" v-bind:style="activeStates.C? itemContainer: itemContainer2">
             <div v-bind:style="activeStates.C? innerContainer: innerContainer2">
-                <img class="dash" src="../../assets/images/feeds.png" />
+                <img class="dash" v-if="activeStates.C" src="../../assets/images/icons/feedWhite.png" />
+                <img class="dash" v-else src="../../assets/images/feeds.png" />
                 <div class="dash-text2" :style="activeStates.C? dashText:dashText2">Feeds</div>
             </div>
         </div>
@@ -35,7 +38,8 @@
     <RouterLink class="route-style" to="/user-management">
         <div @click="userDropAccordion" v-bind:style="activeStates.D? itemContainer: itemContainer2">
             <div @click="user" v-bind:style="activeStates.D? innerContainer: innerContainer2">
-                <img class="dash" src="../../assets/images/user.png" />
+                <img class="dash" v-if="activeStates.D" src="../../assets/images/icons/userWhite.png" />
+                <img class="dash" v-else src="../../assets/images/user.png" />
                 <div class="dash-text2" :style="activeStates.D? dashText:dashText2">User Management</div>
                 <img class="dash-arrow" src="../../assets/images/arrowRight.png" />
             </div>
@@ -50,7 +54,8 @@
     <RouterLink class="route-style" to="/contact-management">
         <div @click="ContactDropAccordion" v-bind:style="activeStates.E? itemContainer: itemContainer2">
             <div @click="contacts" v-bind:style="activeStates.E? innerContainer: innerContainer2">
-                <img class="dash" src="../../assets/images/contact.png" />
+                <img class="dash" v-if="activeStates.E" src="../../assets/images/icons/contactWhite.png" />
+                <img class="dash" v-else src="../../assets/images/contact.png" />
                 <div class="dash-text2" :style="activeStates.E? dashText:dashText2">Contact Management</div>
                 <img style="margin-left: 10%" class="dash-arrow" src="../../assets/images/arrowRight.png" />
             </div>
@@ -65,7 +70,8 @@
     <RouterLink class="route-style" to="/department">
         <div @click="DepartmentDropAccordion" v-bind:style="activeStates.F? itemContainer: itemContainer2">
             <div @click="departments" v-bind:style="activeStates.F? innerContainer: innerContainer2">
-                <img class="dash" src="../../assets/images/department.png" />
+                <img class="dash" v-if="activeStates.F" src="../../assets/images/icons/depWhite.png" />
+                <img class="dash" v-else src="../../assets/images/department.png" />
                 <div class="dash-text2" :style="activeStates.F? dashText:dashText2">Departments</div>
                 <img style="margin-left: 33%" class="dash-arrow" src="../../assets/images/arrowRight.png" />
             </div>
@@ -80,7 +86,8 @@
     <RouterLink class="route-style" to="/shedule">
         <div @click="schedules" v-bind:style="activeStates.G? itemContainer: itemContainer2">
             <div class="inner-container2" v-bind:style="activeStates.G? innerContainer: innerContainer2">
-                <img class="dash" src="../../assets/images/schedule.png" />
+                <img class="dash" v-if="activeStates.G" src="../../assets/images/icons/shedWhite.png" />
+                <img class="dash" v-else src="../../assets/images/schedule.png" />
                 <div class="dash-text2" :style="activeStates.G? dashText:dashText2">Schedule & Reports</div>
             </div>
         </div>
@@ -89,7 +96,8 @@
     <RouterLink class="route-style" to="/inventory">
     <div @click="inventory" v-bind:style="activeStates.H? itemContainer: itemContainer2">
         <div class="inner-container2" v-bind:style="activeStates.H? innerContainer: innerContainer2"> 
-            <img class="dash" src="../../assets/images/inventory.png" />
+            <img class="dash" v-if="activeStates.H" src="../../assets/images/icons/invWhite.png" />
+                <img class="dash" v-else src="../../assets/images/inventory.png" />
             <div class="dash-text2" :style="activeStates.H? dashText:dashText2">Inventory</div>
         </div>
     </div>
@@ -98,7 +106,8 @@
     <RouterLink class="route-style" to="/sales">
         <div @click="sales" v-bind:style="activeStates.I? itemContainer: itemContainer2">
             <div class="inner-container2" v-bind:style="activeStates.I? innerContainer: innerContainer2">
-                <img class="dash" src="../../assets/images/sales.png" />
+                <img class="dash" v-if="activeStates.I" src="../../assets/images/icons/salesWhite.png" />
+                <img class="dash" v-else src="../../assets/images/sales.png" />
                 <div class="dash-text2" :style="activeStates.I? dashText:dashText2">Sales & Payments</div>
             </div>
         </div>
@@ -107,7 +116,8 @@
     <RouterLink class="route-style" to="/companies">
         <div @click="companies" v-bind:style="activeStates.J? itemContainer: itemContainer2">
             <div class="inner-container2" v-bind:style="activeStates.J? innerContainer: innerContainer2">
-                <img class="dash" src="../../assets/images/company.png" />
+                <img class="dash" v-if="activeStates.J" src="../../assets/images/icons/compWhite.png" />
+                <img class="dash" v-else src="../../assets/images/company.png" />
                 <div class="dash-text2" :style="activeStates.J? dashText:dashText2">Companies</div>
             </div>
         </div>
@@ -116,7 +126,8 @@
     <RouterLink class="route-style" to="/request">
     <div @click="requests" v-bind:style="activeStates.K? itemContainer: itemContainer2">
         <div class="inner-container2" v-bind:style="activeStates.K? innerContainer: innerContainer2">
-            <img class="dash" src="../../assets/images/request.png" />
+            <img class="dash" v-if="activeStates.K" src="../../assets/images/icons/reqWhite.png" />
+                <img class="dash" v-else src="../../assets/images/request.png" />
             <div class="dash-text2" :style="activeStates.K? dashText:dashText2">Requests</div>
         </div>
     </div>
@@ -125,7 +136,8 @@
     <RouterLink class="route-style" to="/messaging">
     <div @click="messaging" v-bind:style="activeStates.L? itemContainer: itemContainer2">
         <div class="inner-container2" v-bind:style="activeStates.L? innerContainer: innerContainer2">
-            <img class="dash" src="../../assets/images/messaging.png" />
+            <img class="dash" v-if="activeStates.L" src="../../assets/images/icons/messWhite.png" />
+                <img class="dash" v-else src="../../assets/images/messaging.png" />
             <div class="dash-text2" :style="activeStates.L? dashText:dashText2">messaging</div>
         </div>
     </div>
@@ -134,7 +146,8 @@
     <RouterLink class="route-style" to="/conferencing">
         <div @click="conference" v-bind:style="activeStates.M? itemContainer: itemContainer2">
             <div class="inner-container2" v-bind:style="activeStates.M? innerContainer: innerContainer2">
-                <img class="dash" src="../../assets/images/conferencing.png" />
+                <img class="dash" v-if="activeStates.M" src="../../assets/images/icons/confWhite.png" />
+                <img class="dash" v-else src="../../assets/images/conferencing.png" />
                 <div class="dash-text2" :style="activeStates.M? dashText:dashText2">Conferencing</div>
             </div>
         </div>
